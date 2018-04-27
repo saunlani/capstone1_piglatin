ada8dbb4983c8fb947c2734ca161d2a912c0a658
shaun alan <saunlani@gmail.com>
2018-04-27 19:02:12 -0400

    cleaned up code, added sentence support

--- capstone1_piglatin/Program.cs
+++ capstone1_piglatin/Program.cs
@@ -3,11 +3,21 @@ using System.Text.RegularExpressions;
 
 namespace capstone1_piglatin
 {
-    /* pig latin translator rogram. program takes the first leter at the 
-     * beginning of each word  in a sentencereceived, 
-     * moves it to the end of the wordd for each word in the sentence,
-     * adds an "ay".  ex: "hello world" would be translated to 
-     * "ellohay orldway". */
+    /* pig latin translator program.
+     * ------------------------------------------------------------------------
+     * prompts user for a word .!
+     * translates word to Pig Latin, displays to console .!
+     * asks the user if they want to translate another word. .!
+     * 
+     * deep notes:
+     * converts each word to a lower case .!
+     * if word starts with a vowel, translator just adds "way" to the ending. .!
+     * if word starts with a consonant, all consosants before the first vowel 
+     * are moved to the end of the word, followed by an "ay".
+     * accepts series of words. .!
+     * TODO:  ALLOW PUNCTUATION.
+     * TODO:  KEEP THE CASE.
+     */
 
     class MainClass
     {
@@ -32,55 +42,66 @@ namespace capstone1_piglatin
         static void Translator()
         {
             Console.Write("Enter a line to be translated:  ");
-            string userinput = Console.ReadLine();
-            userinput.ToLower();
+            string rawinput = Console.ReadLine();
+            string userinput = rawinput.ToLower();
+            Console.Write("Translation:  ");
+            string[] individualwords = userinput.Split(' ');
 
-            //grabbing the first two letters
-            string firstletterofword = userinput.Substring(0, 1);
-
-            //grabbing the rest of the word
-            string restofword = userinput.Substring(1, userinput.Length - 1);
-
-            //declaring vowels
-            string vowels = "aeio";
-
-            // index of first vowel appearance
-
-            int firstvowel = 0;
-
-            //checks input for a vowel in firstletterofword and ensures is only 
-            //letters
-            if (vowels.Contains(firstletterofword) && IsOnlyLetters(userinput))
+            foreach (string word in individualwords)
             {
-                Console.WriteLine("Translation: " +userinput + "way");
+                //grabbing the first two letters
+                string firstletterofword = word.Substring(0, 1);
+
+                //grabbing the rest of the word
+                string restofword = word.Substring(1, word.Length - 1);
+
+                //declaring vowels
+                string vowels = "aeiou";
+
+                // index of first vowel appearance
+
+                int firstvowel = 0;
+
+                //checks input for a vowel in firstletterofword and ensures is only 
+                //letters
+                if (vowels.Contains(firstletterofword) && IsOnlyLetters(word))
+                {
+                    Console.Write( word + "way ");
+                }
+                // if input is not only letters, returns false and prints the input
+                // as is.
+                else if (IsOnlyLetters(word) == false)
+                {
+                    Console.Write(word);
+                }
+
+                // checks that vowel is NOT first letter of word (starts with
+                // consonant.  
+                //
+                // moves first letter of word to end of input and adds "ay".
+
+                // TODO: if word starts with a consonant,move all of the consonants that
+                // appear before the first vowel (ex: shrewd = ewd shr ay)
+
+                else if (vowels.Contains(firstletterofword) == false & vowels.Contains(word) == false)
+                {
+                    Console.Write(restofword + firstletterofword + "ay ");
+                }
+                else if (vowels.Contains(firstletterofword) == false)
+                {
+                    firstvowel = word.IndexOfAny(vowels.ToCharArray());
+
+                    string beforevowel = word.Substring(0, firstvowel);
+                    string aftervowel = word.Substring(firstvowel);
+
+                    Console.Write(aftervowel + beforevowel + "ay ");
+                    // Console.WriteLine("Starts with consonant, the index of the first vowel is " + firstvowel);
+                    // Console.WriteLine(restofword + firstletterofword + "ay");
+                }
+                else
+                {}
             }
-            // if input is not only letters, returns false and prints the input
-            // as is.
-            else if (IsOnlyLetters(userinput) == false)
-            {
-                Console.WriteLine(userinput);
-            }
 
-            // checks that vowel is NOT first letter of word (starts with
-            // consonant.  
-            //
-            // moves first letter of word to end of input and adds "ay".
-
-            // TODO: if word starts with a consonant,move all of the consonants that
-            // appear before the first vowel (ex: shrewd = ewd shr ay)
-            else if (vowels.Contains(firstletterofword) == false)
-            {
-                firstvowel = userinput.IndexOfAny(vowels.ToCharArray());
-
-                string beforevowel = userinput.Substring(0, firstvowel);
-                string aftervowel = userinput.Substring(firstvowel);
-
-                Console.WriteLine("Translation: " + aftervowel + beforevowel + "ay");
-                // Console.WriteLine("Starts with consonant, the index of the first vowel is " + firstvowel);
-                // Console.WriteLine(restofword + firstletterofword + "ay");
-            }
-            else
-            { }
 
 
 
@@ -91,7 +112,7 @@ namespace capstone1_piglatin
         {
             do
             {
-                Console.Write("Translate another line? (y/n):  ");
+                Console.Write("\nTranslate another line? (y/n):  ");
                 string userresponse = Console.ReadLine();
                 userresponse.ToLower();
 
